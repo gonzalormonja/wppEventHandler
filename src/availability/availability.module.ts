295f97eb011a7722c83ae0d3bc716eebaece529b
gonzalo monja <gonzalormonja1@gmail.com>
2023-04-19 11:41:41 -0300

    avance in availability

--- src/availability/availability.module.ts
+++ src/availability/availability.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AvailabilityService } from './availability.service';
+import { CalendarModule } from 'src/calendar/calendar.module';
 
 @Module({
+  imports: [CalendarModule],
   providers: [AvailabilityService],
 })
 export class AvailabilityModule {}
--- src/availability/availability.service.ts
+++ src/availability/availability.service.ts
@@ -1,6 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CalendarService } from 'src/calendar/calendar.service';
 
 @Injectable()
 export class AvailabilityService {
-  public async getAvailability() {}
+  constructor(private readonly calendarService: CalendarService) {}
+
+  public async getAvailability(calendarId: string, date: Date) {
+    const calendar = await this.calendarService.getOne(calendarId);
+    if (!calendar) throw new NotFoundException('error.CALENDAR_NOT_FOUND');
+    const inputWeekday = date.getDay();
+    const dateSchedules = calendar.dateSchedules
+      .filter((dateSchedule) => dateSchedule.date == date)
+      .map(({ from, to }) => ({ from, to }));
+    const daySchedules = calendar.daySchedules
+      .filter((daySchedule) => daySchedule.weekday == inputWeekday)
+      .map(({ from, to }) => ({ from, to }));
+    let schedules = [...dateSchedules, ...daySchedules].sort((a, b) =>
+      a.from < b.from ? -1 : 1,
+    );
+    let totalSchedules = schedules.length;
+    let index = 0;
+    while (index < totalSchedules) {
+      if (index > 0) {
+        const currentSchedule = schedules[index];
+        const previousSchedule = schedules[index - 1];
+        if (currentSchedule.from <= previousSchedule.to) {
+          if (currentSchedule.to > previousSchedule.to)
+            schedules[index - 1].to = currentSchedule.to;
+
+          schedules = schedules.slice(index, 1);
+          totalSchedules--;
+        } else {
+          index++;
+        }
+      } else {
+        index++;
+      }
+    }
+    //todo remove bussy schedules
+    return schedules;
+  }
 }
