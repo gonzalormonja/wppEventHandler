bbdcaeebe7f43c168bb3fe37f86d9c9f0f1e8e30
Gonzalo Monja <gonzalormonja1@gmail.com>
2023-05-20 17:30:02 -0300

    nothing

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [forwardRef(() => TypeOrmModule.forFeature([User]))],
   providers: [UserService],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
