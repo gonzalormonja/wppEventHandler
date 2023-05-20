db88798809e53bc0a0912db99f2ee64e2737d641
Gonzalo Monja <gonzalormonja1@gmail.com>
2023-05-20 19:39:17 -0300

    add public to health function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './interceptors/guards/accessToken.guard';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('./../package.json');
 @Controller('')
 export class AppController {
   @Get('/health')
+  @Public()
   public async health() {
     return 'OK';
   }
   @Get('/version')
+  @Public()
   public async version() {
     return version;
   }
