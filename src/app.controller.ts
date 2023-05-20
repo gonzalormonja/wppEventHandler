2746b3d280a687e34023c519eb807d30c266330c
Gonzalo Monja <gonzalormonja1@gmail.com>
2023-05-20 19:34:54 -0300

    modify health and add version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-
-@Controller('app')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('./../package.json');
+@Controller('')
 export class AppController {
   @Get('/health')
   public async health() {
     return 'OK';
+  }
+  @Get('/version')
+  public async version() {
+    return version;
   }
 }
